a4f23f1e055676f4568c33cda30730f2c69bc447
Adrian Niazi <adrian.izi@protonmail.com>
2024-01-24 16:49:44 +0100

    working

--- Exercise 2/exercisego/go.mod
+++ Exercise 2/exercisego/go.mod
@@ -1,3 +1,3 @@
 module exercisego
 
-go 1.21.6
+//go 1.21.6
--- Exercise 2/exercisego/tcpclient/tcpclient.go
+++ Exercise 2/exercisego/tcpclient/tcpclient.go
@@ -10,21 +10,24 @@ import (
 
 //Fixed size messages of size 1024, if you connect to port 34933
 //Delimited messages that use \0 as the marker, if you connect to port 33546
-//Ip for computer at lab is 10.100.23.129
+//Ip for computer at lab is 10.100.23.186
 
 //Tell the server to connect back to you, by sending a message of the form Connect to: #.#.#.#:#\0
 //(IP of your machine and port you are listening to). You can find your own address by running ifconfig in the terminal
 
-func Tcpclient() {
-	istream1 := bufio.NewReader(os.Stdin)
+func Tcpclient(ipAddr string, port string) {
+	/* istream1 := bufio.NewReader(os.Stdin)
 	fmt.Print("What ip address do you want to connect to? ")
-	ipAddr, _ := istream1.ReadString('\n')
+	ipAddr, _ := istream1.ReadString(':')
 
 	istream2 := bufio.NewReader(os.Stdin)
 	fmt.Print("What port do you want to connect to? ")
-	port, _ := istream2.ReadString('\n')
+	port, _ := istream2.ReadString('\n') */
 
-	connection, err := net.Dial("tcp4", ipAddr+":"+port)
+	connectionVar := ipAddr + ":" + port
+	fmt.Println(connectionVar)
+
+	connection, err := net.Dial("tcp", connectionVar)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,7 +38,7 @@ func Tcpclient() {
 		inputbuffer := bufio.NewReader(os.Stdin)
 		fmt.Print("Text to send to server (\"quit\" to close connection): ")
 		text, _ := inputbuffer.ReadString('\n')
-		if text == "quit" {
+		if text == "quit\n" {
 			connection.Close()
 			return
 		}
